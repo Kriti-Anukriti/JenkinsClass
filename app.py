1fc557cf9206a4ca685232f8cc99a4cf67b86122
Kriti-Anukriti <anukriti1302@outlook.com>
2025-10-04 14:11:44 +0530

    Update app.py
    new version

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("hello , this is python project from github")
 print("we are migrating to the new version")
 print("we have the scheduled build by triggers")
+print("moving to the next version")
